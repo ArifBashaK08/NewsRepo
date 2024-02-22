3cb4b00f68a2e9787f0d0a6c8f39bd47cbd6b6fb
Arif Basha K. <arifkolimi08@gmail.com>
2024-02-22 14:32:21 +0530

    NewRepo Version - V3 --> Infinite scroll has  added

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -2,81 +2,89 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem.jsx'
 import Spinner from './Spinner.jsx'
 import PropTypes from 'prop-types'
+import InfiniteScroll from 'react-infinite-scroll-component';
+
 
 export class News extends Component {
 
     static defaultProps = {
         country: 'in',
         category: 'general',
-        pageSize: 25
+        pageSize: 24
     }
 
     static propTypes = {
         country: PropTypes.string,
         category: PropTypes.string,
         pageSize: PropTypes.number
-      };
+    };
 
     constructor() {
         super();
         this.state = {
             articles: [],
-            loading: false,
-            page: 1
+            loading: true,
+            page: 1,
+            totalResults: 0
         }
     }
 
-    async updateNews(){
+    async updateNews() {
         const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&sortBy=popularity&apiKey=${this.props.apiKey}&page=${this.state.page}&pagesize=${this.props.pageSize}`
-        this.setState({ loading: true })
         let data = await fetch(url);
         let parsedData = await data.json();
         console.log(parsedData)
         this.setState({
             articles: parsedData.articles,
-            totalResults: parsedData.totalResults,
-            loading: false
+            totalResults: parsedData.totalResults
         })
     }
 
     async componentDidMount() {
         this.updateNews()
-        }
+    }
 
-    prevPageHandler = async () => {
-        await this.setState({
-            page: this.state.page > 1 ? this.state.page - 1 : this.state.page,
+    fetchMoreData = async () => {
+        // this.setState({page: this.state.page + 1})
+        const nextPage = this.state.page + 1
+        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&sortBy=popularity&apiKey=${this.props.apiKey}&page=${nextPage}&pagesize=${this.props.pageSize}`
+        this.setState({ loading: true })
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        console.log(parsedData)
+        this.setState({
+            articles: this.state.articles.concat(parsedData.articles),
+            totalResults: parsedData.totalResults,
+            page: nextPage,
+            loading: false
         })
-        this.updateNews()
     }
-    
-    nextPageHandler = async () => {
-        await this.setState({
-            page: this.state.page + 1,
-        })
-        this.updateNews()
-    }
+
+
     render() {
         return (
-            <div className='container m-auto my-5'>
-                <h2 className="fw-bold text-light mb-3">{`Top Headlines - ${this.props.category.charAt(0).toUpperCase()+this.props.category.slice(1)}`}</h2>
-                {this.state.loading && <Spinner />}
-                <div className="row row-gap-3">
-                    {this.state.articles && this.state.articles.map((e) => {
-                        return <div className="col-lg-4 col-md-6" key={e.url}>
-                            <NewsItem title={e.title ? e.title : 'Not Available'} description={e.description ? e.description : 'Not Available'} imgUrl={e.urlToImage ? e.urlToImage : 'https://media.istockphoto.com/id/1192070239/photo/abstract-digital-news-concept.webp?b=1&s=170667a&w=0&k=20&c=gh89_KBMRqNn3nhTcZwjIQfsM45NnhWI_k8SDa9A6NM='} newsUrl={e.url} author={e.author} date={e.publishedAt} source={e.source.name} />
+            <>
+                <h2 className="fw-bold text-light text-center mb-4">{`Top Headlines - ${this.props.category.charAt(0).toUpperCase() + this.props.category.slice(1)}`}</h2>
+                {/* {this.state.loading && <Spinner />} */}
+                <InfiniteScroll
+                    dataLength={this.state.articles.length}
+                    next={this.fetchMoreData}
+                    hasMore={this.state.articles.length !== this.state.totalResults}
+                    loader={<Spinner />}>
+                    <div className='container'>
+                        <div className="row row-gap-3">
+                            {this.state.articles.map((e) => {
+                                return <div className="col-lg-4 col-md-6" key={e.url}>
+                                    <NewsItem title={e.title ? e.title : 'Not Available'} description={e.description ? e.description : 'Not Available'} imgUrl={e.urlToImage ? e.urlToImage : 'https://media.istockphoto.com/id/1192070239/photo/abstract-digital-news-concept.webp?b=1&s=170667a&w=0&k=20&c=gh89_KBMRqNn3nhTcZwjIQfsM45NnhWI_k8SDa9A6NM='} newsUrl={e.url} author={e.author} date={e.publishedAt} source={e.source.name} />
+                                </div>
+                            })}
                         </div>
-                    })}
-                    <div className="container d-flex justify-content-between">
-                        <button className="btn btn-dark btn-sm" onClick={this.prevPageHandler} disabled={this.state.page <= 1}>&larr; Prev</button>
-                        <button className="btn btn-dark btn-sm" onClick={this.nextPageHandler} disabled={(this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)) || (this.state.articles > 100)}>Next &rarr;</button>
                     </div>
-
-                </div>
-            </div>
+                </InfiniteScroll>
+            </>
         )
     }
 }
- 
+
 
 export default News
--- src/main.jsx
+++ src/main.jsx
@@ -12,17 +12,19 @@ body.style.backgroundImage = 'linear-gradient(255deg,#1D2B53,#FE7A36)';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
     <BrowserRouter>
-      <Navbar title='NewsRepo'/>
-      <Routes>
-        <Route exact path="/" element={<News key='general' pageSize={pageSize} country='in' category='general' apiKey='417df8ddb39847d6953fef1c6fbfcadb' />} />
-        <Route exact path="/business" element={<News key='business' pageSize={pageSize} country='in' category='business' apiKey='417df8ddb39847d6953fef1c6fbfcadb' />} />
-        <Route exact path="/entertainment" element={<News key='entertainment' pageSize={pageSize} country='in' category='entertainment' apiKey='417df8ddb39847d6953fef1c6fbfcadb' />} />
-        <Route exact path="/health" element={<News key='health' pageSize={pageSize} country='in' category='health' apiKey='417df8ddb39847d6953fef1c6fbfcadb' />} />
-        <Route exact path="/science" element={<News key='science' pageSize={pageSize} country='in' category='science' apiKey='417df8ddb39847d6953fef1c6fbfcadb' />} />
-        <Route exact path="/sports" element={<News key='sports' pageSize={pageSize} country='in' category='sports' apiKey='417df8ddb39847d6953fef1c6fbfcadb' />} />
-        <Route exact path="/technology" element={<News key='technology' pageSize={pageSize} country='in' category='technology' apiKey='417df8ddb39847d6953fef1c6fbfcadb' />} />
-        {/* <Route path="/About"><About />} /> */}
-      </Routes>
+      <Navbar title='NewsRepo' />
+      <section className='container m-auto my-4'>
+        <Routes>
+          <Route exact path="/" element={<News key='general' pageSize={pageSize} country='in' category='general' apiKey='417df8ddb39847d6953fef1c6fbfcadb' />} />
+          <Route exact path="/business" element={<News key='business' pageSize={pageSize} country='in' category='business' apiKey='417df8ddb39847d6953fef1c6fbfcadb' />} />
+          <Route exact path="/entertainment" element={<News key='entertainment' pageSize={pageSize} country='in' category='entertainment' apiKey='417df8ddb39847d6953fef1c6fbfcadb' />} />
+          <Route exact path="/health" element={<News key='health' pageSize={pageSize} country='in' category='health' apiKey='417df8ddb39847d6953fef1c6fbfcadb' />} />
+          <Route exact path="/science" element={<News key='science' pageSize={pageSize} country='in' category='science' apiKey='417df8ddb39847d6953fef1c6fbfcadb' />} />
+          <Route exact path="/sports" element={<News key='sports' pageSize={pageSize} country='in' category='sports' apiKey='417df8ddb39847d6953fef1c6fbfcadb' />} />
+          <Route exact path="/technology" element={<News key='technology' pageSize={pageSize} country='in' category='technology' apiKey='417df8ddb39847d6953fef1c6fbfcadb' />} />
+          {/* <Route path="/About"><About />} /> */}
+        </Routes>
+      </section>
     </BrowserRouter>
   </>,
 );
